35c0145b4945d858453dc95fb9455da6fa28ca81
Sonya <sofya.beldy@mail.ru>
2023-04-17 21:07:26 +1000

    First commit

---
+++ src/main/java/ru/sonyabeldy/hibernate/App.java
@@ -0,0 +1,13 @@
+package ru.sonyabeldy.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
