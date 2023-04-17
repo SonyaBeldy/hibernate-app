e0e91af13b295444fa6ed36b5bb1885abbf30d25
Sonya <sofya.beldy@mail.ru>
2023-04-17 21:38:50 +1000

    Hibernate configuration without Spring, create Person

--- src/main/java/ru/sonyabeldy/hibernate/App.java
+++ src/main/java/ru/sonyabeldy/hibernate/App.java
@@ -1,5 +1,10 @@
 package ru.sonyabeldy.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.sonyabeldy.hibernate.model.Person;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 1);
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
